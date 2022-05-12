365f87266727464cb689733018899a0e73afbb4c
Egi Fajar Nasrulloh <78475349+egifn@users.noreply.github.com>
2022-05-12 13:48:48 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var nama;
-nama = prompt('Halo boleh kenalan ?'); 
-confirm('Apakah kamu yakin ?');
-if (nama == "agin") 
-{
-    alert('Halo ' + nama + ' sayang <3<3<3 aaegi sayang sama kamu');
-} else {
-    alert('Aku tidak mengenalmu !')
-}
