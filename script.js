5822ae8d4c4b41b60be185c1d5eec1854ddec8c7
Egi Fajar Nasrulloh <78475349+egifn@users.noreply.github.com>
2022-05-12 13:49:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var nama;
+nama = prompt('Halo, Nama Kamu Siapa ?'); 
+confirm('Apakah kamu yakin ?');
+if (nama == "agin") 
+{
+    alert('Halo ' + nama + ' sayang <3<3<3 aa egi sayang sama kamu');
+} else {
+    alert('Aku tidak mengenalmu !')
+}
